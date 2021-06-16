2cc121938c9ccbaef02b8962382c0ceba45143c6
Jake Costen <master.scratchman@gmail.com>
2021-06-16 12:36:17 +0100

    imported eigen and wrote class def

---
+++ engine/src/FeedForwardNeuralNet.cpp
@@ -0,0 +1,26 @@
+#include "FeedForwardNeuralNet.h"
+
+
+void FeedForwardNeuralNet::input(float *) {
+    
+}
+
+float * FeedForwardNeuralNet::compute(int) {
+    
+}
+
+void FeedForwardNeuralNet::randomise(int) {
+    
+}
+
+float * FeedForwardNeuralNet::getWeights(int, int) {
+    
+}
+
+float FeedForwardNeuralNet::getBias(int, int) {
+    
+}
+
+float FeedForwardNeuralNet::cost(float *) {
+    
+}
---
+++ engine/src/FeedForwardNeuralNet.h
@@ -0,0 +1,26 @@
+#include "../../deps/eigen-3.3.9/Eigen/Core"
+#include <vector>
+
+using namespace Eigen;
+using namespace std;
+
+class Layer {
+public:
+    Matrix<float, Dynamic, Dynamic> weights;
+    Matrix<float, Dynamic, 1> biases;
+};
+
+class FeedForwardNeuralNet {
+private:
+    Matrix<float, Dynamic, 1> inputData;
+    vector<Layer> layers;
+public:
+    void input (float *);
+    float * compute (int);
+
+    void randomise (int);
+    float * getWeights (int, int);
+    float getBias (int, int);
+
+    float cost (float *);
+};
